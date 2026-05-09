37790daefd2495c6df2eb7ed1d5430a8e151a198
David Y. Stephenson <david@davidystephenson.com>
2026-05-09 12:25:30 -0400

    Preview comment vote update logic

--- src/pages/User/ThreadPage.jsx
+++ src/pages/User/ThreadPage.jsx
@@ -13,6 +13,28 @@ export default function ThreadPage({ thread, goBack }) {
     fetchCommentsForThread(thread._id).then(setThreadComments);
   }, [thread._id]);
 
+  function upvote (commentId) {
+    const newComments = threadComments.map((comment) => {
+      if (comment._id !== commentId) return comment;
+      const alreadyUpvoted = comment.upvotedBy.includes('currentUserId');
+      if (alreadyUpvoted) {
+        const newComment = {
+          ...comment,
+          upvotedBy: comment.upvotedBy.filter(id => id !== 'currentUserId'),
+          voteCount: comment.voteCount - 1
+        }
+        return newComment;
+      }
+      const newComment = {
+        ...comment,
+        upvotedBy: [...comment.upvotedBy, 'currentUserId'],
+        voteCount: comment.voteCount + 1
+      }
+      return newComment;
+    })
+    setThreadComments(newComments);
+  }
+
   return (
     <Container className="thread-container">
       <ThreadCard
@@ -20,7 +42,10 @@ export default function ThreadPage({ thread, goBack }) {
         goBack={goBack}
       />
       <CommentForm />
-      <CommentList comments={threadComments} />
+      <CommentList
+        comments={threadComments}
+        upvote={upvote}
+      />
     </Container>
   );
 }
